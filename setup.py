01b1cfbb3d92ffc922eb282a6cecfb3fdae42136
ElmerDotti <42123943+ElmerDotti@users.noreply.github.com>
2018-08-05 19:24:42 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,44 +1,45 @@
-#
-#  HOW TO UPLOAD PROJECT TO PYPI REPOSITORY
-#
-#  $ python setup.py sdist upload -r pypi
-#
+from setuptools import find_packages, setup
 
-from distutils.core import setup
+# read the contents of README file
+from os import path
+from io import open  # for Python 2 and 3 compatibility
+
+exec(open(path.join('pyod', 'version.py')).read())
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# read the contents of requirements.txt
+with open(path.join(this_directory, 'requirements.txt'),
+          encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
 setup(
-        name='pytrain',
-        version='0.0.12',
-        packages = [
-            'pytrain',
-            'pytrain.lib',
-            'pytrain.KNN',
-            'pytrain.LinearRegression', 
-            'pytrain.LogisticRegression',
-            'pytrain.GaussianNaiveBayes', 
-            'pytrain.NaiveBayes',
-            'pytrain.DecisionTreeID3', 
-            'pytrain.Kmeans',
-            'pytrain.DBSCAN', 
-            'pytrain.Apriori',
-            'pytrain.HierarchicalClustering',
-            'pytrain.HMM',
-            'pytrain.CRF',
-            'pytrain.NeuralNetwork',
-            'pytrain.SVM'
-            ],
-        include_package_data=True,
-        package_data={'':['*.eng']},
-        author ='becxer',
-        author_email='becxer87@gmail.com',
-        url = 'https://github.com/becxer/pytrain',
-        description ='Machinelearning library for python',
-        long_description ='Machinelearning library for python',
-        license='MIT',
-        install_requires=['numpy'],
-        classifiers=[
-                'License :: OSI Approved :: MIT License',
-                'Programming Language :: Python :: 2.7',
-        ]
+    name='pyod',
+    version=__version__,
+    description='A Python Outlier Detection (Anomaly Detection) Toolbox',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Yue Zhao',
+    author_email='yuezhao@cs.toronto.edu',
+    url='https://github.com/yzhao062/Pyod',
+    download_url='https://github.com/yzhao062/Pyod/archive/master.zip',
+    keywords=['outlier detection', 'anomaly detection', 'outlier ensembles',
+              'data mining'],
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    install_requires=requirements,
+    setup_requires=['setuptools>=38.6.0'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
-
